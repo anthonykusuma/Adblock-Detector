80ac916cfa19c24f2aba33570ff8cb6fe66e3de7
Anthony Kusuma <anthony.kusuma@live.com>
2019-04-09 16:45:54 +0930

    Initial release

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Adblock Detector
+A simple adblock using javascript
+
+## How It Works
+
+## How To Use
+
---
+++ adsbygoogle.js
@@ -0,0 +1,5 @@
+/*
+It's just an external javascript for displaying ads div in the page
+*/
+var canRunAds = true;
+document.getElementById('realAds').style.display='block';
